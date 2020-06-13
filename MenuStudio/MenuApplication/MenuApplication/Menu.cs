cb8c9d647ac8a383beeee30c13ce969998903c2b
Melissa B <missmelissaboots@gmail.com>
2020-06-13 15:01:52 -0400

    chap4studio

---
+++ MenuStudio/MenuApplication/MenuApplication/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuApplication
+{
+    class Menu
+    {
+        public DateTime LastUpdated { get; }
+        public List<MenuItem> MenuItems { get; set; }
+
+        public Menu (DateTime dateUpdated, List<MenuItem> menuItem)
+        {
+            LastUpdated = dateUpdated;
+            MenuItems = menuItem;
+        }
+    }
+}
---
+++ MenuStudio/MenuApplication/MenuApplication/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuApplication
+{
+    class MenuItem
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+        public MenuItem (double price, string description, string category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+        }
+    }
+}
